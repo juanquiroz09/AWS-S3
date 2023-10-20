bff40e385cd2b26ff4cd043e87750cb48da42eaa
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-10-20 01:28:38 -0500

    use_state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
+import {useState} from 'react'
 function App() {
+  conts [postMessage, setPost] = useState({
+    title: '',
+    photo: null
+  })
   return (
-    <div>App</div>
+    <div>
+      <form>
+        <input type="text" placeholder="title"
+        onChange={e => setPost({...postMessage, title: e.target.value})}/>
+        <input type="file" name="photo"
+        onChange={e => console.log(e)}/>
+        <button>Upload</button>
+      </form>
+    </div>
   )
 }
 
